de1b48961e7668e240bb4f7bbe9d8ac760905d03
ByronAlexis20 <byronalexisramirez20@gmail.com>
2021-08-24 19:44:33 -0500

    cambio

---
+++ src/com/actividades/controlador/seguridad/CambioCargoC.java
@@ -0,0 +1,80 @@
+package com.actividades.controlador.seguridad;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.GlobalCommand;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Window;
+
+import com.actividades.modelo.Empleado;
+import com.actividades.modelo.EmpleadoDAO;
+
+public class CambioCargoC {
+	public String textoBuscar;
+	private EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+	private List<Empleado> listaEmpleados;
+	private Empleado empleadoSeleccionado;
+	
+	@AfterCompose
+	public void aferCompose(@ContextParam(ContextType.VIEW) Component view) throws IOException{
+		Selectors.wireComponents(view, this, false);
+		textoBuscar="";
+		buscar();
+	}
+	@Command
+	public void cambioCargo(){
+		if(empleadoSeleccionado == null) {
+			Clients.showNotification("Seleccione una opción de la lista.");
+			return;
+		}
+		// Actualiza la instancia antes de enviarla a editar.
+		empleadoDAO.getEntityManager().refresh(empleadoSeleccionado);		
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("Empleado", empleadoSeleccionado);
+		Window ventanaCargar = (Window) Executions.createComponents("/formularios/seguridad/empleado/motivoDespido.zul", null, params);
+		ventanaCargar.doModal();
+	}
+	@GlobalCommand("Empleado.buscarEmpleado")
+	@Command
+	@NotifyChange({"listaEmpleados", "empleadoSeleccionado"})
+	public void buscar(){
+		if (listaEmpleados != null) {
+			listaEmpleados = null; 
+		}
+		listaEmpleados = empleadoDAO.buscarEmpleados(textoBuscar);
+		if(listaEmpleados.size() == 0) {
+			Clients.showNotification("No hay datos para mostrar.!!");
+		}else {
+			empleadoSeleccionado = null;
+		}
+	}
+	public String getTextoBuscar() {
+		return textoBuscar;
+	}
+	public void setTextoBuscar(String textoBuscar) {
+		this.textoBuscar = textoBuscar;
+	}
+	public List<Empleado> getListaEmpleados() {
+		return listaEmpleados;
+	}
+	public void setListaEmpleados(List<Empleado> listaEmpleados) {
+		this.listaEmpleados = listaEmpleados;
+	}
+	public Empleado getEmpleadoSeleccionado() {
+		return empleadoSeleccionado;
+	}
+	public void setEmpleadoSeleccionado(Empleado empleadoSeleccionado) {
+		this.empleadoSeleccionado = empleadoSeleccionado;
+	}
+}
---
+++ src/com/actividades/controlador/seguridad/NuevoCargoC.java
@@ -0,0 +1,112 @@
+package com.actividades.controlador.seguridad;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.zkoss.bind.BindUtils;
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Window;
+
+import com.actividades.modelo.Cargo;
+import com.actividades.modelo.CargoDAO;
+import com.actividades.modelo.Empleado;
+import com.actividades.modelo.EmpleadoDAO;
+import com.actividades.util.Constantes;
+import com.actividades.util.PrintReport;
+
+public class NuevoCargoC {
+	@Wire Combobox cboCargo;
+	@Wire Window winNuevoCargo;
+	
+	private CargoDAO cargoDAO = new CargoDAO();
+	private List<Cargo> cargos;
+	Cargo cargoSeleccionado;
+	
+	Empleado empleado;
+	EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+	
+	@AfterCompose
+	public void afterCompose(@ContextParam(ContextType.VIEW) Component view) {
+		Selectors.wireComponents(view, this, false);
+		// Recupera el objeto pasado como parametro. 
+		empleado = (Empleado) Executions.getCurrent().getArg().get("Empleado");
+	}
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Command
+	public void grabar(){
+		
+		Messagebox.show("Desea realizar el despido?", "Confirmación", Messagebox.YES | Messagebox.NO, Messagebox.QUESTION, new EventListener() {
+			@Override
+			public void onEvent(Event event) throws Exception {
+				if (event.getName().equals("onYes")) {		
+					try {						
+						empleadoDAO.getEntityManager().getTransaction().begin();
+						imprimirActividades(empleado);
+						empleado.setFechaSalida(new Date());
+						empleado.setEstado("I");
+						empleadoDAO.getEntityManager().merge(empleado);
+						empleadoDAO.getEntityManager().getTransaction().commit();
+						Clients.showNotification("Proceso Ejecutado con exito.");
+						BindUtils.postGlobalCommand(null, null, "Empleado.buscarEmpleado", null);
+						salir();						
+					} catch (Exception e) {
+						e.printStackTrace();
+						empleadoDAO.getEntityManager().getTransaction().rollback();
+					}
+				}
+			}
+		});
+	}	
+	
+	private void imprimirActividades(Empleado emp) {
+		if(emp.getTipoUsuario().getIdTipoUsuario() == Constantes.ID_CARGO_JEFE) {
+			Map<String, Object> params = new HashMap<String, Object>();
+			params.put("ID_TIPO_ACTIVIDAD", Constantes.ID_TIPO_PRIMORDIALES);
+			params.put("FECHA_INICIO", emp.getFechaIngreso());
+			params.put("FECHA_FIN", new Date());
+			params.put("FECHA_BUSQUEDA", "Fecha de contrato: Del " + new SimpleDateFormat("dd/MM/yyyy").format(emp.getFechaIngreso()) + " hasta " + new SimpleDateFormat("dd/MM/yyyy").format(new Date()));
+			params.put("ID_EMPLEADO", emp.getIdEmpleado());
+			PrintReport report = new PrintReport();
+			report.crearReporte("/reportes/actividades.jasper",empleadoDAO, params);
+		}
+	}
+	public Empleado getEmpleado() {
+		return empleado;
+	}
+	public void setEmpleado(Empleado empleado) {
+		this.empleado = empleado;
+	}
+	@Command
+	public void salir(){
+		winNuevoCargo.detach();
+	}
+	public List<Cargo> getCargos() {
+		List<Cargo> lista = this.cargoDAO.getCargosActivos();
+		return cargos;
+	}
+	public void setCargos(List<Cargo> cargos) {
+		this.cargos = cargos;
+	}
+	public Cargo getCargoSeleccionado() {
+		return cargoSeleccionado;
+	}
+	public void setCargoSeleccionado(Cargo cargoSeleccionado) {
+		this.cargoSeleccionado = cargoSeleccionado;
+	}
+	
+}
